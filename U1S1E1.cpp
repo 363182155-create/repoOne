e396cddb78f232357a5962d8b4acbf7502fd93ef
ChoMoneyChoProblems <363182155@gotvdsb.ca>
2026-09-11 08:45:25 -0400

    if statement

--- U1S1E1.cpp
+++ U1S1E1.cpp
@@ -16,9 +16,9 @@ int main() {
     ticket = rand() %200 + 1;
 
 printf("|------------------------------------|\n");
-printf("|Name: %s |\n", name.c_str());
-printf("|Section: %c |\n", letters[rand() % 4]);
-printf("|Seat: %d |\n", ticket);
+printf("|%-25s |\n", name.c_str());
+printf("|Section: %-1c                        |\n", letters[rand() % 4]);
+printf("|Seat: %03d |\n", ticket);
 printf("|Price: $%d |\n", price);
 printf("|------------------------------------|\n");
 
--- U1S1E2.cpp
+++ U1S1E2.cpp
@@ -1,1 +1,31 @@
 #include <iostream>
+
+using namespace std;
+
+int main() {
+    string name; 
+    int ticket;
+    int price = rand() %51 + 50;
+
+    char letters[] = {'A', 'B', 'C', 'D'};
+
+    cout << "What is your name? " << endl;
+    getline(cin, name);
+
+    if (name.length() > 25)
+    {
+        name = name.substr(0, 25);
+    }
+    
+    srand(time(0));
+    ticket = rand() %200 + 1;
+
+printf("|------------------------------------|\n");
+printf("|%-25s |\n", name.c_str());
+printf("|Section: %-1c                        |\n", letters[rand() % 4]);
+printf("|Seat: %03d                          |\n", ticket);
+printf("|Price: $%2d |\n", price);
+printf("|------------------------------------|\n");
+
+    return 0;
+}

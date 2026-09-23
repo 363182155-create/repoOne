e252d4605362eb1bfdd4a6450cd1f4f4af5f91a1
ChoMoneyChoProblems <363182155@gotvdsb.ca>
2026-09-23 09:05:33 -0400

    done

--- U1S3E3.cpp
+++ U1S3E3.cpp
@@ -11,7 +11,7 @@ void datatype(string str)
     
     // Strip sign for easier digit counting and parsing
     string cleanStr = str;
-    if (str[0] == '-' || str[0] == '+') 
+    if (str[0] == '-' || str[0] == '+')  // Check for negative or positive sign
     {
         cleanStr = str.substr(1);
     }
@@ -37,12 +37,75 @@ void datatype(string str)
         return;
     }
 
-    else if (cleanStr.length() == 1) 
+    else if (cleanStr.length() == 1)  //if the length of the string is 1, it is a char
     {
         cout << "Char" << endl;
         return;
     }
-}
+
+    try // Convert to a large integer type to check bounds safely
+    {
+        long long value = stoll(str); // Convert string to long long
+
+        if (isNegative) // Negative numbers and checks if the value is within each of the data type ranges
+        {
+            if (value >= SHRT_MIN)
+            {
+                cout << "Short" << endl;
+            } 
+            else if (value >= INT_MIN) 
+            {
+                cout << "Long" << endl;
+            } 
+            else 
+            {
+                cout << "Long long" << endl;
+            }
+        } 
+
+        else 
+        {
+            // Positive numbers and checks if the value is within each of the data type ranges 
+            if (value <= SHRT_MAX) 
+            {
+                cout << "Short" << endl;
+            } 
+            else if (value <= USHRT_MAX) 
+            {
+                cout << "Unsigned short" << endl;
+            } 
+            else if (value <= INT_MAX) 
+            {
+                cout << "Int" << endl;
+            } 
+            else if (value <= UINT_MAX) 
+            {
+                cout << "Unsigned int" << endl;
+            }
+            else 
+            {
+                cout << "unsigned long long" << endl;
+            }
+        }
+    }
+
+    catch (const out_of_range&) // The value is too large for long long
+    {
+        if (isNegative) 
+        {
+            cout << "long long" << endl;
+        }
+        else
+        {
+            cout << "unsigned long long" << endl;
+        }
+    }
+
+    catch (const invalid_argument&) // The string is not a valid number
+    {
+        cout << "Invalid input" << endl;
+    }
+}    
 
 
 int main()
@@ -53,4 +116,6 @@ int main()
 
     cout << "Data Type: ";
     datatype(number);
+
+    return 0;
 }
